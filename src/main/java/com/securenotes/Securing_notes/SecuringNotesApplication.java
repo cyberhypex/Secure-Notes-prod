b1782f33ed469207ee530f6f4f8a8bc1e57fc978
Anshuman <chaoanxuman999@gmail.com>
2025-01-07 15:51:21 +0530

    Starting secure notes application project

---
+++ src/main/java/com/securenotes/Securing_notes/SecuringNotesApplication.java
@@ -0,0 +1,13 @@
+package com.securenotes.Securing_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securenotes/Securing_notes/SecuringNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securenotes.Securing_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
