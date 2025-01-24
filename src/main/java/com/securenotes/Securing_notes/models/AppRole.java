5b06c0e1bdc28a32ce5d82c5461ed8bb03d122ea
Anshuman <chaoanxuman999@gmail.com>
2025-01-24 21:33:52 +0530

    Roles and Custom User Model Added TODO:Check Foreign Key Referencing

---
+++ src/main/java/com/securenotes/Securing_notes/models/AppRole.java
@@ -0,0 +1,6 @@
+package com.securenotes.Securing_notes.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/securenotes/Securing_notes/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.securenotes.Securing_notes.repositories;
+
+
+import com.securenotes.Securing_notes.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
